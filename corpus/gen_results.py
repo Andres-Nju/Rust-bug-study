7075610a971a2495392d314780aacd33a793371d
Andres-Nju <1572378310@qq.com>
2023-12-04 23:19:35 +0800

    12.4

--- corpus/gen_results.py
+++ corpus/gen_results.py
@@ -24,12 +24,15 @@ len_panic_count = df[df['symptom'] == 3]['len_panic'].value_counts()
 # 7. review coexistence of different causes and panic
 cause_panic_count = df[df['symptom'] == 3]['root_cause'].value_counts()
 
+# 8. 为每个symptom生成每种root_cause的数量
+symptom_root_cause_count = df.groupby(['symptom', 'root_cause']).size().unstack(fill_value=0)
 
 # 保存结果到新的CSV文件
-repo_root_cause_count.to_csv('repo_root_cause_count.csv')
+#repo_root_cause_count.to_csv('repo_root_cause_count.csv')
 symptom_count.to_csv('symptom_count.csv')
 avg_code_change_by_root_cause.to_csv('avg_code_change_by_root_cause.csv')
 error_handling_count.to_csv('error_handling_count.csv')
 propagation_chain_count.to_csv('propagation_chain_count.csv')
 len_panic_count.to_csv('len_panic_count.csv')
 cause_panic_count.to_csv('cause_panic_count.csv')
+#symptom_root_cause_count.to_csv('symptom_root_cause_count.csv')
